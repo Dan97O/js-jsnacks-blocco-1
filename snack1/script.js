1c2e650876e94faed969be455a44d84383c1f505
Dan97O <dan97-oddo@live.it>
2023-03-14 11:42:18 +0100

    add es 1

---
+++ snack1/script.js
@@ -0,0 +1,10 @@
+const numberUser = Number(prompt('Inserisci primo numero'));
+const numberUs = Number(prompt('Inserisci secondo numero'));
+
+console.log(numberUser, numberUs);
+
+if(numberUser > numberUs){
+    console.log(numberUser);
+} else if (numberUs > numberUser){
+    console.log(numberUs)
+} 
