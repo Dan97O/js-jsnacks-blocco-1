665e7735c361d1fcf6b3747c2832a1dff1a6f4a7
Dan97O <dan97-oddo@live.it>
2023-03-14 12:22:31 +0100

    add firstword log

--- snack2/script.js
+++ snack2/script.js
@@ -7,11 +7,9 @@ const secondWord = prompt('inserisci la seconda parola');
 
 if(firstWord.length > secondWord.length){
     console.log(secondWord.length, "la seconda parola è piu corta");
-
     console.log(firstWord.length, "la prima parola è piu lunga");
 } else if (secondWord.length > firstWord.length){
-    console.log(secondWord.length, "la prima parola è piu corta");
-
+    console.log(firstWord.length, "la prima parola è piu corta");
     console.log(secondWord.length, "la seconda parola è piu lunga")
 } else {
     console.log("le parole hanno stessa lunghezza")
