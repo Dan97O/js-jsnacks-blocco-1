4eabf69b9b482ba58069e0632632c2b80edd1a74
Dan97O <dan97-oddo@live.it>
2023-03-14 12:10:02 +0100

    add es 2

---
+++ snack2/script.js
@@ -0,0 +1,15 @@
+/* L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga. */
+
+
+const firstWord = prompt('inserisci la prima parola');
+const secondWord = prompt('inserisci la seconda parola');
+
+if(firstWord.length > secondWord.length){
+    console.log(firstWord.length, "la prima parola è piu lunga");
+} else if (secondWord.length > firstWord.length){
+    console.log(secondWord.length, "la seconda parola è piu lunga")
+} else {
+    console.log("le parole hanno stessa lunghezza")
+}
+
