22bf885903075460cf73d5632b1d2e9fadf84318
Dan97O <dan97-oddo@live.it>
2023-03-15 11:50:16 +0100

    add es 3 with while

--- snack3/main.js
+++ snack3/main.js
@@ -2,10 +2,22 @@
 Il programma stampa la somma di tutti i numeri inseriti. */
 
 
-
+/* 
 for (let i = 0; i <= 10; i++) {
     const numerUser = Number(prompt('inserisci un numero'));
     const somma = numerUser + i
     console.log(somma); 
-  }
+  } */
   
+
+  //Con while
+
+  let sum = 0;
+  let i = 0;
+  while (i < 10) {
+    let numb = Number(prompt('inserisci un numero'))
+    sum += numb
+    i++    
+  }
+  console.log(sum); 
+
